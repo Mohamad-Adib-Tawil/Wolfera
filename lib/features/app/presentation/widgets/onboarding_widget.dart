d8c0e387b03e468ec314981bc143c3a391ea8509
Mohamad Adib Tawil <mat.07.mat.70.mat@gmail.com>
2025-10-05 18:49:26 +0300

    Add Language Dropdown

--- lib/features/app/presentation/widgets/onboarding_widget.dart
+++ lib/features/app/presentation/widgets/onboarding_widget.dart
@@ -13,6 +13,7 @@ import 'package:wolfera/core/config/theme/typography.dart';
 import 'package:wolfera/core/utils/extensions/build_context.dart';
 import 'package:wolfera/features/app/presentation/widgets/app_elvated_button.dart';
 import 'package:wolfera/features/app/presentation/widgets/app_text.dart';
+import 'package:wolfera/features/app/presentation/widgets/language_dropdown.dart';
 import 'package:wolfera/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:wolfera/generated/assets.dart';
 import 'package:wolfera/generated/locale_keys.g.dart';
@@ -32,88 +33,100 @@ class OnboardingPage extends StatelessWidget {
   Widget body(BuildContext context) {
     return BlocProvider.value(
         value: GetIt.I<AuthBloc>(),
-        child: Column(
-          children: [
-            55.verticalSpace,
-            Padding(
-              padding: HWEdgeInsets.symmetric(horizontal: 28),
-              child: Bounce(
-                duration: const Duration(milliseconds: 1300),
-                child: SimpleShadow(
-                  color: AppColors.orange,
-                  offset: const Offset(0, 4),
-                  opacity: 0.2,
-                  sigma: 4,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              20.verticalSpace,
+              Padding(
+                padding: HWEdgeInsets.only(right: 20, left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const LanguageDropdown(),
+                  ],
+                ),
+              ),
+              35.verticalSpace,
+              Padding(
+                padding: HWEdgeInsets.symmetric(horizontal: 28),
+                child: Bounce(
+                  duration: const Duration(milliseconds: 1300),
+                  child: SimpleShadow(
+                    color: AppColors.orange,
+                    offset: const Offset(0, 4),
+                    opacity: 0.2,
+                    sigma: 4,
+                    child: AppText(
+                      LocaleKeys.onboarding_welcomeTowolferaCars,
+                      style: context.textTheme.bodyLarge?.xb.s40
+                          .withColor(AppColors.orange),
+                    ),
+                  ),
+                ),
+              ),
+              60.verticalSpace,
+              FadeInLeft(
+                duration: const Duration(milliseconds: 1400),
+                child: Image.asset(
+                  Assets.imagesOnboarding,
+                ),
+              ),
+              50.verticalSpace,
+              Padding(
+                padding: HWEdgeInsets.symmetric(horizontal: 28),
+                child: Bounce(
                   child: AppText(
-                    LocaleKeys.onboarding_welcomeTowolferaCars,
-                    style: context.textTheme.bodyLarge?.xb.s40
+                    LocaleKeys.onboarding_buyAndSellCarsEffortlessly,
+                    style: context.textTheme.headlineMedium?.xb
                         .withColor(AppColors.orange),
                   ),
                 ),
               ),
-            ),
-            80.verticalSpace,
-            FadeInLeft(
-              duration: const Duration(milliseconds: 1400),
-              child: Image.asset(
-                Assets.imagesOnboarding,
-              ),
-            ),
-            50.verticalSpace,
-            Padding(
-              padding: HWEdgeInsets.symmetric(horizontal: 28),
-              child: Bounce(
-                child: AppText(
-                  LocaleKeys.onboarding_buyAndSellCarsEffortlessly,
-                  style: context.textTheme.headlineMedium?.xb
-                      .withColor(AppColors.orange),
+              15.verticalSpace,
+              Padding(
+                padding: HWEdgeInsetsDirectional.only(start: 5),
+                child: ShakeX(
+                  duration: const Duration(milliseconds: 1300),
+                  child: AppText(
+                    LocaleKeys.onboarding_YourPerfectRideisJustATapAway,
+                    style: context.textTheme.bodyLarge?.s20.sb,
+                  ),
                 ),
               ),
-            ),
-            15.verticalSpace,
-            Padding(
-              padding: HWEdgeInsetsDirectional.only(start: 5),
-              child: ShakeX(
-                duration: const Duration(milliseconds: 1300),
-                child: AppText(
-                  LocaleKeys.onboarding_YourPerfectRideisJustATapAway,
-                  style: context.textTheme.bodyLarge?.s20.sb,
+              25.verticalSpace,
+              FadeInDownBig(
+                curve: Curves.easeOut,
+                child: AppElevatedButton(
+                  appButtonStyle: AppButtonStyle.primary,
+                  style: ButtonStyle(
+                      backgroundColor:
+                          const WidgetStatePropertyAll(AppColors.white),
+                      minimumSize: WidgetStatePropertyAll(Size(319.w, 54.h))),
+                  onPressed: () =>
+                      context.push(GRouter.config.authRoutes.signupPage),
+                  text: LocaleKeys.auth_createAccount,
+                  textStyle: context.textTheme.titleLarge.xb
+                      .withColor(AppColors.blackLight),
                 ),
               ),
-            ),
-            50.verticalSpace,
-            FadeInDownBig(
-              curve: Curves.easeOut,
-              child: AppElevatedButton(
-                appButtonStyle: AppButtonStyle.primary,
-                style: ButtonStyle(
-                    backgroundColor:
-                        const WidgetStatePropertyAll(AppColors.white),
-                    minimumSize: WidgetStatePropertyAll(Size(319.w, 54.h))),
-                onPressed: () =>
-                    context.push(GRouter.config.authRoutes.signupPage),
-                text: LocaleKeys.auth_createAccount,
-                textStyle: context.textTheme.titleLarge.xb
-                    .withColor(AppColors.blackLight),
+              20.verticalSpace,
+              FadeInDownBig(
+                curve: Curves.easeOut,
+                child: AppElevatedButton(
+                  appButtonStyle: AppButtonStyle.primary,
+                  style: ButtonStyle(
+                      backgroundColor:
+                          const WidgetStatePropertyAll(AppColors.white),
+                      minimumSize: WidgetStatePropertyAll(Size(319.w, 54.h))),
+                  onPressed: () =>
+                      context.push(GRouter.config.authRoutes.loginPage),
+                  text: LocaleKeys.auth_login,
+                  textStyle: context.textTheme.titleLarge.xb
+                      .withColor(AppColors.blackLight),
+                ),
               ),
-            ),
-            40.verticalSpace,
-            FadeInDownBig(
-              curve: Curves.easeOut,
-              child: AppElevatedButton(
-                appButtonStyle: AppButtonStyle.primary,
-                style: ButtonStyle(
-                    backgroundColor:
-                        const WidgetStatePropertyAll(AppColors.white),
-                    minimumSize: WidgetStatePropertyAll(Size(319.w, 54.h))),
-                onPressed: () =>
-                    context.push(GRouter.config.authRoutes.loginPage),
-                text: LocaleKeys.auth_login,
-                textStyle: context.textTheme.titleLarge.xb
-                    .withColor(AppColors.blackLight),
-              ),
-            ),
-          ],
+            ],
+          ),
         ));
   }
 }
