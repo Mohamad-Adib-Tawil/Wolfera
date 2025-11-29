307e494398f269ac203922442c9b949454c8f1d5
Mohamad Adib Tawil <mohamad.adib.tawil@gmail.com>
2025-11-29 11:57:18 +0300

    تعديل باقي الحقول

--- lib/features/my_car/presentation/widgets/car_logo_item.dart
+++ lib/features/my_car/presentation/widgets/car_logo_item.dart
@@ -11,29 +11,53 @@ class CarLogoItem extends StatelessWidget {
     required this.svgColor,
     this.isSelected = false,
     this.onTap,
+    this.label,
   });
 
   final String assetPath;
   final Color? svgColor;
   final bool isSelected;
   final VoidCallback? onTap;
+  final String? label;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: HWEdgeInsets.all(14),
+        padding: HWEdgeInsets.all(10),
         decoration: BoxDecoration(
-            color: Colors.grey[800],
-            borderRadius: BorderRadius.circular(8.r),
-            border: isSelected
-                ? Border.all(color: AppColors.primary, width: 1.2.r)
-                : null),
-        child: AppSvgPicture(
-          assetPath,
-          fit: BoxFit.contain,
-          color: svgColor,
+          color: Colors.grey[800],
+          borderRadius: BorderRadius.circular(8.r),
+          border: isSelected
+              ? Border.all(color: AppColors.primary, width: 1.2.r)
+              : null,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: AppSvgPicture(
+                assetPath,
+                fit: BoxFit.contain,
+                color: svgColor,
+              ),
+            ),
+            if (label != null) ...[
+              SizedBox(height: 6.h),
+              Text(
+                label!,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.white,
+                  fontSize: 11.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ],
         ),
       ),
     );
--- lib/features/my_car/presentation/widgets/car_logo_sliver_grid.dart
+++ lib/features/my_car/presentation/widgets/car_logo_sliver_grid.dart
@@ -1,10 +1,8 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:wolfera/common/enums/car_makers.dart';
 import 'package:wolfera/core/config/theme/colors_app.dart';
 import 'package:wolfera/core/utils/responsive_padding.dart';
 import 'package:wolfera/features/my_car/presentation/widgets/car_logo_item.dart';
-import 'package:wolfera/generated/assets.dart';
 
 class CarLogoSliverGrid extends StatelessWidget {
   final List<CarMaker> selectedCarMakers;
@@ -27,39 +25,26 @@ class CarLogoSliverGrid extends StatelessWidget {
           crossAxisSpacing: 16,
           mainAxisSpacing: 16,
         ),
-        delegate: SliverChildListDelegate(
-          [
-            CarLogoItem(
-                onTap: () => onMakerSelected(CarMaker.mercedes),
-                isSelected: selectedCarMakers.contains(CarMaker.mercedes),
-                assetPath: Assets.makersMercedes,
-                svgColor: AppColors.white),
-            CarLogoItem(
-                onTap: () => onMakerSelected(CarMaker.bmw),
-                isSelected: selectedCarMakers.contains(CarMaker.bmw),
-                assetPath: Assets.makersBmwWhiteBlack,
-                svgColor: null),
-            CarLogoItem(
-                onTap: () => onMakerSelected(CarMaker.volkswagen),
-                isSelected: selectedCarMakers.contains(CarMaker.volkswagen),
-                assetPath: Assets.makersVolkswagen,
-                svgColor: AppColors.white),
-            CarLogoItem(
-                onTap: () => onMakerSelected(CarMaker.honda),
-                isSelected: selectedCarMakers.contains(CarMaker.honda),
-                assetPath: Assets.makersHonda,
-                svgColor: AppColors.white),
-            CarLogoItem(
-                onTap: () => onMakerSelected(CarMaker.jeep),
-                isSelected: selectedCarMakers.contains(CarMaker.jeep),
-                assetPath: Assets.makersJeep,
-                svgColor: AppColors.white),
-            CarLogoItem(
-                onTap: () => onMakerSelected(CarMaker.audi),
-                isSelected: selectedCarMakers.contains(CarMaker.audi),
-                assetPath: Assets.makersAudi,
-                svgColor: AppColors.white),
-          ],
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            final maker = CarMaker.values[index];
+            // Some logos work better without a forced color. Use null to keep original.
+            final forceWhite = <CarMaker>{
+              CarMaker.mercedes,
+              CarMaker.volkswagen,
+              CarMaker.honda,
+              CarMaker.audi,
+              CarMaker.jeep,
+            };
+            return CarLogoItem(
+              onTap: () => onMakerSelected(maker),
+              isSelected: selectedCarMakers.contains(maker),
+              assetPath: maker.logoAsset,
+              svgColor: forceWhite.contains(maker) ? AppColors.white : null,
+              label: maker.name,
+            );
+          },
+          childCount: CarMaker.values.length,
         ),
       ),
     );
